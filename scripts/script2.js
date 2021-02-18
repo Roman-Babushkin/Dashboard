66845b9f7958a4f886a58b41ed3ae1d75a17e67c
Roman Babushkin <69390256+Roman-Babushkin@users.noreply.github.com>
2021-02-18 16:49:57 +0300

    Add files via upload

---
+++ scripts/script2.js
@@ -0,0 +1,7 @@
+document.getElementById("back").onclick = function() {
+  window.location.href = 'index.html';
+}
+
+document.getElementById("obj1").onclick = function() {
+	window.location.href = 'object1.html';
+}
