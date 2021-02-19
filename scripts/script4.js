58dabdccba25dfb9cc18ba14e78fe06568b53e27
Roman Babushkin <69390256+Roman-Babushkin@users.noreply.github.com>
2021-02-19 11:30:44 +0300

    Add files via upload

--- scripts/script4.js
+++ scripts/script4.js
@@ -1,3 +1,30 @@
 document.getElementById("back").onclick = function() {
   window.location.href = 'gkpir.html';
 }
+
+var time = new Date();
+
+var start = new Date(2021, 0, 1);
+
+let a = Math.round((time-start)/86400000);
+
+console.log(a);
+
+let b = a/3.65;
+
+let c= b+7;
+
+let nowTime = document.getElementById('now');
+
+nowTime.style.marginLeft = b+'%';
+
+let nowDate = document.getElementById('nowDate');
+
+nowDate.style.marginLeft = c+'%';
+
+let d = time.getDate();
+let m = time.getMonth()+1;
+let y = time.getFullYear();
+let fullDate = d+'.'+m+'.'+y;
+
+nowDate.innerHTML = fullDate;
