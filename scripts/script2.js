4e40df7adb187d0e6bd13fc7dd7355fffa846ba6
Roman Babushkin <69390256+Roman-Babushkin@users.noreply.github.com>
2021-02-25 16:27:16 +0300

    Add files via upload

--- scripts/script2.js
+++ scripts/script2.js
@@ -10,12 +10,22 @@ var osvPir32 = 42;
 
 var lboPir27 = 2100;
 
+let massiveColor = {
+  1: [0, 1, 1, 1, 1, 1],
+  2: [0, 0, 2, 3, 4, 4],
+  3: [0, 0, 1, 3, 3, 4],
+  4: [0, 1, 2, 3, 3, 4],
+  5: [0, 3, 3, 4, 4, 4],
+  6: [0, 0, 2, 2, 4, 4],
+  7: [0, 0, 2, 4, 4, 4]
+}
+
 document.getElementById("back").onclick = function() {
   window.location.href = 'index.html';
 }
 
 document.getElementById("obj1").onclick = function() {
-	window.location.href = 'object1.html';
+  window.location.href = 'object1.html';
 }
 
 document.getElementById("contrPir27").innerHTML = contrPir27 + "%";
@@ -24,9 +34,9 @@ document.getElementById("osvPir27").innerHTML = osvPir27;
 
 document.getElementById("contrPir32").innerHTML = contrPir32 + "%";
 
-document.getElementById("osvPir32").innerHTML = osvPir32 + "%"; 
+document.getElementById("osvPir32").innerHTML = osvPir32 + "%";
 
-document.getElementById("lboPir27").innerHTML = lboPir27 + '<br><span class="typeP2"> млн.рублей</span>'; 
+document.getElementById("lboPir27").innerHTML = lboPir27 + '<br><span class="typeP2"> млн.рублей</span>';
 
 document.getElementById("lineContr27").style.background = "linear-gradient(to right, rgb(54, 85, 138) " + contrPir27 + "%, lightgrey " + contrPir27 + "%)";
 
@@ -37,3 +47,28 @@ document.getElementById("lineOsv27").style.background = "linear-gradient(to righ
 document.getElementById("lineContr32").style.background = "linear-gradient(to right, rgb(37, 155, 209) " + contrPir32 + "%, lightgrey " + contrPir32 + "%)";
 
 document.getElementById("lineOsv32").style.background = "linear-gradient(to right, rgb(37, 155, 209) " + osvPir32 + "%, lightgrey " + osvPir32 + "%)";
+
+function colors() {
+  for (h = 1; h < 8; h++) {
+    exp = "obj" + h;
+    x = document.getElementById(`${exp}`);
+    for (i = 1; i < 7; i++) {
+      el = "kt" + i
+      col = x.querySelector(`.${el}`);
+      ob = massiveColor[h];
+      if (ob[i - 1] === 0) {
+        col.style.background = "white";
+      } else if (ob[i - 1] === 1) {
+        col.style.background = "rgb(54, 85, 138)";
+      } else if (ob[i - 1] === 2) {
+        col.style.background = "rgb(37, 155, 209)";
+      } else if (ob[i - 1] === 3) {
+        col.style.background = "rgba(37, 155, 209, 0.2)";
+      } else {
+        col.style.background = "lightgrey";
+      }
+    }
+  }
+}
+
+colors();
--- scripts/script3.js
+++ scripts/script3.js
@@ -10,6 +10,16 @@ var osvSmr32 = 37;
 
 var lboSmr27 = 3500;
 
+let massiveColorSmr = {
+  1: [0, 1, 1, 1, 1, 1],
+  2: [0, 1, 2, 3, 3, 4],
+  3: [0, 1, 1, 2, 2, 4],
+  4: [0, 1, 3, 4, 4, 4],
+  5: [0, 2, 2, 3, 3, 4],
+  6: [0, 1, 1, 1, 4, 4],
+  7: [0, 0, 0, 4, 4, 4]
+}
+
 document.getElementById("back").onclick = function() {
   window.location.href = 'index.html';
 }
@@ -33,3 +43,28 @@ document.getElementById("lineOsv27").style.background = "linear-gradient(to righ
 document.getElementById("lineContr32").style.background = "linear-gradient(to right, rgb(37, 155, 209) " + contrSmr32 + "%, lightgrey " + contrSmr32 + "%)";
 
 document.getElementById("lineOsv32").style.background = "linear-gradient(to right, rgb(37, 155, 209) " + osvSmr32 + "%, lightgrey " + osvSmr32 + "%)";
+
+function colors() {
+  for (h = 1; h < 8; h++) {
+    exp = "obj" + h;
+    x = document.getElementById(`${exp}`);
+    for (i = 1; i < 8; i++) {
+      el = "kt" + i
+      col = x.querySelector(`.${el}`);
+      ob = massiveColorSmr[h];
+      if (ob[i - 1] === 0) {
+        col.style.background = "white";
+      } else if (ob[i - 1] === 1) {
+        col.style.background = "rgb(54, 85, 138)";
+      } else if (ob[i - 1] === 2) {
+        col.style.background = "rgb(37, 155, 209)";
+      } else if (ob[i - 1] === 3) {
+        col.style.background = "rgba(37, 155, 209, 0.2)";
+      } else {
+        col.style.background = "lightgrey";
+      }
+    }
+  }
+}
+
+colors();
